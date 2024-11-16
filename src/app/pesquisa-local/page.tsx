99e831ea82d44ce4ed347a777a734d01b633c73e
pedroonovais <phenrique101007@gmail.com>
2024-11-16 18:48:11 -0300

    Termina pagina de locais

--- src/app/pesquisa-local/page.tsx
+++ src/app/pesquisa-local/page.tsx
@@ -1,19 +1,19 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
 
 interface Local {
-    ID_LOCAL: number;
-    NOME_LOCAL: string;
-    CATEGORIA: string;
-    LOGRADOURO_NUMERO_LOCAL: string;
-    CEP_LOCAL: string;
-    CIDADE_LOCAL: string;
-    ESTADO_LOCAL: string;
-    LATITUDE_LOCAL: string;
-    LONGITUDE_LOCAL: string;
+    id: string;
+    nome: string;
+    categoria: string;
+    logradouro: string;
+    cep: string;
+    cidade: string;
+    estado: string;
+    latitude: string;
+    longitude: string;
 }
 
 export default function PesquisaLocais() {
@@ -21,6 +21,27 @@ export default function PesquisaLocais() {
     const [locais, setLocais] = useState<Local[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
+
+    const apiUrl = process.env.NEXT_PUBLIC_API_BACKEND_URL;
+
+    const carregaLocais = async () => {
+        setLoading(true);
+        setError("");
+        try {
+            console.log(`Url = ${process.env.NEXT_PUBLIC_API_BACKEND_URL}/local`);
+            const res = await fetch(`${apiUrl}/local`);
+            if (!res.ok) {
+                throw new Error("Falha ao carregar os locais");
+            }
+            const data: Local[] = await res.json();
+            setLocais(data);
+            console.log(data);
+        } catch (err) {
+            setError("Ocorreu um erro ao buscar os locais.");
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const handlePesquisar = async () => {
         if (!localizacao) {
@@ -43,6 +64,10 @@ export default function PesquisaLocais() {
         }
     };
 
+    useEffect(() => {
+        carregaLocais();
+    }, []);
+
     return (
         <div
             className="relative min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
@@ -50,7 +75,7 @@ export default function PesquisaLocais() {
         >
             <div className="absolute inset-0 bg-black opacity-50"></div>
 
-            <div className="relative z-10 max-w-5xl w-full p-8 bg-white bg-opacity-20 backdrop-blur-md rounded-lg">
+            <div className="relative z-10 max-w-5xl w-full p-8 my-8 bg-white bg-opacity-20 backdrop-blur-md rounded-lg">
                 <div className="flex justify-center mb-8">
                     <Link href="/">
                         <Image
@@ -87,38 +112,39 @@ export default function PesquisaLocais() {
                             </>
                         )}
                     </button>
+
                 </div>
                 {error && <p className="text-red-500 text-center mt-4">{error}</p>}
 
+                <div className="relative z-10 mt-6 text-center">
+                    <Link href="/" className="text-white underline hover:text-verdeClaro">
+                        Retornar para página inicial
+                    </Link>
+                </div>
+
                 {locais.length > 0 && (
-                    <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                    <div className="mt-10 w-full">
                         {locais.map((local) => (
                             <div
-                                key={local.ID_LOCAL}
-                                className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition duration-300"
+                                key={local.id}
+                                className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 mb-6"
                             >
                                 <h3 className="text-xl font-semibold text-verdeFolha">
-                                    {local.NOME_LOCAL}
+                                    {local.nome}
                                 </h3>
                                 <p className="text-cinzaPedregulho mt-2">
-                                    {local.CATEGORIA}
+                                    {local.categoria}
                                 </p>
                                 <p className="text-cinzaPedregulho mt-2">
-                                    {local.LOGRADOURO_NUMERO_LOCAL}
+                                    {local.logradouro}
                                 </p>
                                 <p className="text-cinzaPedregulho mt-2">
-                                    {local.CIDADE_LOCAL} - {local.ESTADO_LOCAL}
+                                    {local.cidade} - {local.estado}
                                 </p>
                             </div>
                         ))}
                     </div>
                 )}
-            </div>
-
-            <div className="relative z-10 mt-6">
-                <Link href="/" className="text-white underline hover:text-verdeClaro">
-                    Retornar para página inicial
-                </Link>
             </div>
         </div>
     );
