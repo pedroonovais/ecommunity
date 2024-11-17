e1dc1d25ca2112f2a2b5c7849284b5d2d82e48dd
pedroonovais <phenrique101007@gmail.com>
2024-11-17 18:30:18 -0300

    Altera arquivos do projeto

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:slug*', 
-          destination: 'http://179.111.200.106:8888/api/:slug*',
-        },
-      ]
-    },
-  }
-  
-  module.exports = nextConfig
-  
+const nextConfig = {}
+
+module.exports = nextConfig
--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -22,10 +22,6 @@ export default function Noticias() {
 
     const carregarNoticias = async () => {
         try {
-            console.log(`${apiUrl}/materia`);
-            return;
-            
-
             const res = await fetch(`${apiUrl}/materia`);
             if (!res.ok) {
                 throw new Error("Erro ao carregar notícias");
