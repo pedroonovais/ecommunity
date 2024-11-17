734cdab1889e62eaf62635fb02df5ee6df1f0328
pedroonovais <phenrique101007@gmail.com>
2024-11-17 17:51:26 -0300

    Edita o next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:slug*', 
+          destination: 'http://179.111.200.106:8888/api/:slug*',
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
+  
